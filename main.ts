d6cf8b6faa6e90fd8c791e2e704b2d0191354a3b
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-11 13:17:36 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+let day= "saturday";
+
+switch (day){
+    case "Monday":
+            console.log("Its your work day, do your assignments.");
+            break;
+
+    case "sunday":
+        console.log("Its your off, have fun.")
+        break;
+
+        case "saturday":
+        console.log("Its your class at Governor house by 9am.")
+        break;
+
+        case "Friday":
+            console.log("Today is friday, manage some time for prayers.");
+            break;
+
+            default:
+                console.log("Invalid input");
+                    
+}
